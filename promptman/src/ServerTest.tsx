6cab34cdcbd50e4354d0a7439b3dd26ac1fd73b6
Howard Pinsley <howard.pinsley@blackstone.com>
2025-01-29 07:49:20 -0500

    Add cors to the server and test that the client can indeed contact the server.

--- promptman/src/ServerTest.tsx
+++ promptman/src/ServerTest.tsx
@@ -4,14 +4,27 @@ interface ServerTestProps {
 }
 
 const ServerTest: React.FC<ServerTestProps> = (props) => {
-    const contactServer = () => {
-      console.log("Contacting server");
-    }
+  const contactServer = async () => {
+    try {
+      console.log("Attempting fetch");
 
-    return (
-      <div>
-        <button onClick={() => contactServer()}>Contact Server</button>
-      </div>);
+      // const response = await fetch("http://host.docker.internal:8080");
+      const response = await fetch("http://localhost:8080");
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const text = await response.text()
+      console.log(text);
+
+    } catch (error) {
+      console.error('There was a problem with the fetch operation:', error);
+    }
   };
+
+  return (
+    <div>
+      <button onClick={() => contactServer()}>Contact Server</button>
+    </div>);
+};
 
 export default ServerTest;
--- server/src/app.ts
+++ server/src/app.ts
@@ -1,10 +1,13 @@
 import express, { Request, Response } from 'express';
+import cors from 'cors';
 
 const app = express();
 const port = 8080;
 
+app.use(cors());
+
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
+  res.send('Hello there World!');
 });
 
 app.listen(port, () => {
