21f79dc96fd6e964105b4af66c4bbeba1ba3e456
Howard Pinsley <howard.pinsley@blackstone.com>
2025-02-03 11:40:18 -0500

    Create a client-side workflow service

--- promptman/src/Workflows.tsx
+++ promptman/src/Workflows.tsx
@@ -1,21 +1,29 @@
 import React, { useEffect } from 'react';
-import { getDefaultWorkflowIdAsync } from './services/workflow_service';
+import { getDefaultWorkflowIdAsync, getAllWorkflowIdsAsync } from './services/workflow_service';
 
 interface WorkflowProps {
 }
 
 const Workflows: React.FC<WorkflowProps> = () => {
   const [defaultWorkflowId, setDefaultWorkflowId] = React.useState('');
+  const [workflowIds, setWorkflowIds] = React.useState<string[]>([]);
 
   useEffect(() => {
       getDefaultWorkflowIdAsync()
         .then((defaultWorkflowId) => setDefaultWorkflowId(defaultWorkflowId));
+
+      getAllWorkflowIdsAsync()
+        .then((workflowIds) => setWorkflowIds(workflowIds));
+      
   }, []);
 
   return (
     <div>
       <h1>Workflows</h1>
       <p>Default Workflow ID: {defaultWorkflowId}</p>
+      <ul>
+        { workflowIds.map((workflowId) => <li key={workflowId}>{workflowId}</li>) }
+      </ul>
     </div>
   );
 
--- promptman/src/services/workflow_service.ts
+++ promptman/src/services/workflow_service.ts
@@ -1,4 +1,5 @@
 import { PROMPTMAN_SERVICE_URL } from '../config';
+import { Workflow } from '../models/WorkflowModels';
 
 export const getDefaultWorkflowIdAsync = async (): Promise<string> => {
     const url = `${PROMPTMAN_SERVICE_URL}/workflows/default-workflow/id`;
@@ -8,3 +9,14 @@ export const getDefaultWorkflowIdAsync = async (): Promise<string> => {
     const defaultWorkflowId = await response.text();
     return defaultWorkflowId;
 };
+
+export const getAllWorkflowIdsAsync = async (): Promise<string[]> => {
+  const url = `${PROMPTMAN_SERVICE_URL}/workflows`;
+  console.log(`url: ${url}`);
+
+  const response = await fetch(url);
+  const workflows:Workflow[] = await response.json();
+  const workflowIds = workflows.map(w => w.id);
+  return workflowIds;
+};
+
