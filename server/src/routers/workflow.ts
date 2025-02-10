34f403f2f774abd7a7c50ed43526c4f7c480a3bf
Howard Pinsley <howard.pinsley@blackstone.com>
2025-02-10 07:37:17 -0500

    Restore the update default workflow route.  I accidentally removed it yesterday when reorganizing the order of the methods.

--- server/src/routers/workflow.ts
+++ server/src/routers/workflow.ts
@@ -5,41 +5,54 @@ import { Workflow, WorkflowStage } from '../models/workflow/workflow_models';
 const workflowRouter = Router();
 
 
-workflowRouter.get('/', async (req:any, res:any) => {
-    // Access the 'objective' from the request body
-    const workflows = getWorkflows();
-    res.json(workflows);
-  });
-
-  workflowRouter.get('/default-workflow', async (req:any, res:any) => {
-    // Access the 'objective' from the request body
-    const defaultWorkflow = getDefaultWorkflow();
-    res.json(defaultWorkflow);
-  });
-  
-  workflowRouter.get('/default-workflow/id', async (req:any, res:any) => {
-    // Access the 'objective' from the request body
-    const defaultWorkflowId = getDefaultWorkflowId();
-    res.type("text/plain");
-    res.send(defaultWorkflowId);
-  });
-
-workflowRouter.get('/:workflowId', async (req:any, res:any) => {
-    // Access the 'objective' from the request body
-    const workflowId = req.params.workflowId
-    const workflow = find_workflow_by_id(workflowId);
-    if (!workflow) {
-      res.status(404).json({ message: `Workflow with ID ${workflowId} not found` });
-      return;
-    }
-    
-    res.json(workflow)
+workflowRouter.get('/', async (req: any, res: any) => {
+  // Access the 'objective' from the request body
+  const workflows = getWorkflows();
+  res.json(workflows);
 });
 
-workflowRouter.put('/:workflowId', async (req: express.Request<{workflowId:string}, Workflow, Workflow>, res:any) => {
+workflowRouter.get('/default-workflow', async (req: any, res: any) => {
+  // Access the 'objective' from the request body
+  const defaultWorkflow = getDefaultWorkflow();
+  res.json(defaultWorkflow);
+});
+
+workflowRouter.post('/default-workflow/id/:newDefaultWorkflowId', async (req, res) => {
+  // Extract the 'newDefaultWorkflowId' from the request parameters
+  const newDefaultWorkflowId = req.params.newDefaultWorkflowId;
+  const succeeded = setDefaultWorkflowId(newDefaultWorkflowId);
+
+  if (!succeeded) {
+    res.status(404).send(`Workflow with ID ${newDefaultWorkflowId} not found`);
+    return;
+  }
+
+  res.status(200).send(`Default workflow ID set to ${newDefaultWorkflowId}`);
+});
+
+workflowRouter.get('/default-workflow/id', async (req: any, res: any) => {
+  // Access the 'objective' from the request body
+  const defaultWorkflowId = getDefaultWorkflowId();
+  res.type("text/plain");
+  res.send(defaultWorkflowId);
+});
+
+workflowRouter.get('/:workflowId', async (req: any, res: any) => {
+  // Access the 'objective' from the request body
+  const workflowId = req.params.workflowId
+  const workflow = find_workflow_by_id(workflowId);
+  if (!workflow) {
+    res.status(404).json({ message: `Workflow with ID ${workflowId} not found` });
+    return;
+  }
+
+  res.json(workflow)
+});
+
+workflowRouter.put('/:workflowId', async (req: express.Request<{ workflowId: string }, Workflow, Workflow>, res: any) => {
   const workflowId = req.params.workflowId
   if (!workflowId) {
-    res.status(400).json({ message: ':workflowId is required'})
+    res.status(400).json({ message: ':workflowId is required' })
     return;
   }
 
@@ -48,7 +61,7 @@ workflowRouter.put('/:workflowId', async (req: express.Request<{workflowId:strin
     res.status(404).json({ message: `Workflow with ID ${workflowId} not found` });
     return;
   }
-  
+
   res.json(workflow)
 });
 
